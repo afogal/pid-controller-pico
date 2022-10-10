f3f1a35e46783dedf0d2a09ba771dfe2e84872c6
James D. D. Martin <jddmartin@uwaterloo.ca>
2022-10-10 13:50:49 -0400

    Clarify origin and purpose of "boot.py"

--- pico/boot.py
+++ pico/boot.py
@@ -1,19 +1,29 @@
-"""CircuitPython Essentials Storage logging boot.py file"""
+"""Control access to storage
+
+Paraphrasing: https://learn.adafruit.com/circuitpython-essentials/circuitpython-storage
+"This "boot.py" file executes on a hard reset or powering up the board.
+It is not run on soft reset, e.g., reloading the board from the serial console
+or the REPL. This is in contrast to the code within code.py, which is executed after CircuitPython is already running."
+
+Following the adafruit example (link above), we check the status of a digital
+input.  If it is held low then circuitpython can write to storage.  Otherwise,
+the computer connected via the usb serial port can write to storage.
+
+Note that even if you cannot write to CIRCUITPY over USB, you will be able
+to read.
+"""
+
 import board
 import digitalio
 import storage
 
-# For Gemma M0, Trinket M0, Metro M0/M4 Express, ItsyBitsy M0/M4 Express
 switch = digitalio.DigitalInOut(board.GP0)
-
-# For Feather M0/M4 Express
-# switch = digitalio.DigitalInOut(board.D5)
-
-# For Circuit Playground Express, Circuit Playground Bluefruit
-# switch = digitalio.DigitalInOut(board.D7)
-
 switch.direction = digitalio.Direction.INPUT
 switch.pull = digitalio.Pull.UP
 
-# If the switch pin is connected to ground CircuitPython can write to the drive
+# allow CircuitPython to write to drive if input is grounded:
+if switch.value:
+    print("circuitpython can NOT write to storage, USB CAN")
+else:
+    print("circuitpython can write to storage, USB can NOT")
 storage.remount("/", switch.value)
